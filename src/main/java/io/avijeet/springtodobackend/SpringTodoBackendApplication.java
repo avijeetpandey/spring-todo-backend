695a47e280c93e85d13d6a3fccbd154ba0b60095
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-08-25 22:50:45 +0530

    Add: intial code

---
+++ src/main/java/io/avijeet/springtodobackend/SpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package io.avijeet.springtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/avijeet/springtodobackend/SpringTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.avijeet.springtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
